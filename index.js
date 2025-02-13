b759e726f1baeb37ec32c9688f7f1c513ccedeba
istiakAHMEDsaad <istik2020@gmail.com>
2025-02-14 02:56:50 +0600

    start

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-console.log('hola');
+// import module
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+// Cors
+const port = process.env.PORT || 3000;
+const app = express();
+app.use(cors());
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@my-mongodb.2rdes.mongodb.net/?retryWrites=true&w=majority&appName=My-MongoDB`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    const db = client.db('car-rental-system');
+    const carsCollection = db.collection('cars');
+    
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+app.get('/', (req, res) => {
+    res.send({ "car rental system server": "server is running..." });
+  });
+  
+  app.listen(port, () => console.log(`Server running on port ${port}`));
