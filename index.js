c486f3f15d112d4ea1bc273146a71fb766c4c5e8
istiakAHMEDsaad <istik2020@gmail.com>
2025-02-14 15:13:19 +0600

    post all car and get data

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 // import module
-require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 
-// Cors
 const port = process.env.PORT || 3000;
 const app = express();
-app.use(cors());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+app.use(cors());
+app.use(express.json());
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@my-mongodb.2rdes.mongodb.net/?retryWrites=true&w=majority&appName=My-MongoDB`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -17,27 +18,43 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    const db = client.db('car-rental-system');
-    const carsCollection = db.collection('cars');
-    
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const db = client.db('car-rental-system');
+    const carsCollection = db.collection('cars');
+
+    // --> save car information in database <--
+    app.post('/add-car', async (req, res) => {
+      const carData = req.body;
+      const result = await carsCollection.insertOne(carData);
+      console.log(result);
+      res.send(result);
+    });
+
+    // --> get car information from db <--
+    app.get('/all-car', async (req, res) => {
+      const result = await carsCollection.find().toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    await client.db('admin').command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 app.get('/', (req, res) => {
-    res.send({ "car rental system server": "server is running..." });
-  });
-  
-  app.listen(port, () => console.log(`Server running on port ${port}`));
+  res.send({ 'car rental system server': 'server is running...' });
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
